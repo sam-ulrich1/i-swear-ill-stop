37aa809336204ae214c73df4e48775a8d0ffb8ee
Sam Ulrich <samson98ulrich@gmail.com>
2022-10-11 09:52:43 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,56 @@
+<h1 align="center">
+  <br>
+  I Swear I'll Stop
+  <br>
+</h1>
+
+<h4 align="center">My girlfriend wants me to stop swearing. Let's ask <a href="https://github.com/openai/whisper" target="_blank">Whisper</a> for some help.</h4>
+
+<p align="center">
+  <a href="#key-features">Key Features</a> •
+  <a href="#how-to-use">How To Use</a> •
+  <a href="#credits">Credits</a> •
+  <a href="#license">License</a>
+</p>
+
+## Key Features
+
+* Real-time audio processing from raw microphone input
+* OpenAI Whisper inference
+* GPU & CPU support
+* Audio alarm and DBus system notifications
+
+## How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com) and [Python](https://www.python.org/) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/sam-ulrich1/i-swear-ill-stop
+
+# Go into the repository
+$ cd i-swear-ill-stop
+
+# Install dependencies
+$ sudo apt install build-essential libdbus-glib-1-dev libgirepository1.0-dev ffmpeg portaudio19-dev
+$ pip install -r requirements.txt
+
+# Run the app
+$ python main.py
+```
+
+## Credits
+
+This software uses the following open source packages:
+
+- [OpenAI Whisper](https://github.com/openai/whisper)
+- [whisper_mic](https://github.com/mallorbc/whisper_mic)
+- [SpeechRecognition](https://github.com/Uberi/speech_recognition)
+- [PyAudio](https://people.csail.mit.edu/hubert/pyaudio)
+- [Bad Words File](https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/blob/master/en)
+- <a href="https://www.flaticon.com/free-icons/swear" title="swear icons">Swear icons created by Smashicons - Flaticon</a>
+
+## License
+
+MIT
+
---
+++ main.py
@@ -0,0 +1,154 @@
+import io
+import queue
+import re
+import threading
+import time
+import vlc
+import dbus
+
+from pydub import AudioSegment
+import speech_recognition as sr
+import whisper
+import tempfile
+import os
+import click
+
+
+@click.command()
+@click.option("--model", default="tiny", help="Model to use",
+              type=click.Choice(["tiny", "base", "small", "medium", "large"]))
+@click.option("--english", default=False, help="Whether to use English model", is_flag=True, type=bool)
+@click.option("--verbose", default=False, help="Whether to print verbose output", is_flag=True, type=bool)
+@click.option("--device", default=None, help="GPU device to run the Whisper model on", type=int)
+@click.option("--bad-words-path", default="bad_words.txt", help="Path to badwords file", type=str)
+@click.option("--alarm", default="alarm.mp3", help="Alarm sound path", type=str)
+@click.option("--sample-rate", default=64_000, help="Sample rate of audio", type=int)
+@click.option("--sample-buffer-size", default=3, help="Sample buffer for maximum amount of input that can persist", type=float)
+@click.option("--alert-icon", default="swear.png", help="Path to icon for swear bot notifications", type=str)
+def main(model, english, verbose, device, bad_words_path, alarm, sample_rate,
+         sample_buffer_size, alert_icon):
+    alert_icon = os.path.abspath(alert_icon)
+    alarm = os.path.abspath(alarm)
+
+    if model != "large" and english:
+        model = model + ".en"
+    audio_model = whisper.load_model(model, device=f"cuda:{device}" if device is not None else "cpu")
+
+    # load bad words
+    with open(bad_words_path) as f:
+        BAD_WORDS = [x for x in f.read().split("\n") if len(x) > 0]
+
+    q = queue.Queue(maxsize=-1)
+
+    with sr.Microphone(sample_rate=sample_rate) as source:
+        def process_sample():
+            buffer = []
+            timestamps = []
+
+            bad_word_alerts = []
+
+            while True:
+                audio_data, timestamp = q.get(block=True, timeout=None)
+
+                if verbose:
+                    print("Queue Size: ", q.qsize())
+
+                buffer.append(audio_data)
+                timestamps.append(timestamp)
+                if len(buffer) > sample_buffer_size * 2:
+                    buffer.pop(0)
+                    timestamps.pop(0)
+
+                raw_audio_data = b''.join(buffer)
+                data = io.BytesIO(sr.AudioData(raw_audio_data, source.SAMPLE_RATE, source.SAMPLE_WIDTH).get_wav_data())
+                audio_clip = AudioSegment.from_file(data)
+
+                temp_audio_path = tempfile.mktemp(suffix=".wav")
+
+                audio_clip.export(temp_audio_path, format="wav")
+
+                if english:
+                    result = audio_model.transcribe(temp_audio_path, language='english')
+                else:
+                    result = audio_model.transcribe(temp_audio_path)
+
+                try:
+                    os.remove(temp_audio_path)
+                except:
+                    pass
+
+                if verbose:
+                    print("Raw Result: ", result)
+
+                predicted_text = result["text"]
+
+                if len(predicted_text) == 0:
+                    continue
+
+                cleaned_text = re.sub(r'[^\w\s]', '', predicted_text.lower())
+
+                words = cleaned_text.split()
+
+                if verbose:
+                    print("Words; ", words)
+
+                if len(words) == 0:
+                    continue
+
+                bad_words = []
+                for word_idx, word in enumerate(words):
+                    for bw in BAD_WORDS:
+                        if word == bw:
+                            bad_words.append((word_idx, word))
+                            break
+
+                if verbose:
+                    print("Bad Words: ", bad_words)
+
+                if len(bad_words) == 0:
+                    continue
+
+                bad_words = [x[1] for x in bad_words]
+                bad_word_alerts = [ts for ts in bad_word_alerts if ts >= timestamps[0]]
+
+                alerted = False
+                for ts in timestamps:
+                    if ts in bad_word_alerts:
+                        if verbose:
+                            print("Skipping for existing alert")
+                        alerted = True
+                        break
+
+                if alerted:
+                    continue
+
+                if len(bad_words) == 0:
+                    continue
+
+                bad_word_alerts = timestamps
+
+                if alarm == "none":
+                    os.system("beep -f 555 -l 460")
+                else:
+                    # load alarm
+                    alarm_player = vlc.MediaPlayer(alarm)
+                    alarm_player.play()
+
+                notify = dbus.Interface(
+                    dbus.SessionBus().get_object("org.freedesktop.Notifications", "/org/freedesktop/Notifications"),
+                    "org.freedesktop.Notifications"
+                )
+                notify.Notify("Swear Bot", 42069, alert_icon, "Swear Alert",
+                              f"You said {'a bad word' if len(bad_words) == 1 else 'some bad words'}: {', '.join(bad_words)}",
+                              [], [], 10000
+                )
+
+        threading.Thread(target=process_sample, daemon=True).start()
+
+        while True:
+            data = source.stream.read(sample_rate // 2)
+
+            q.put((data, time.time()))
+
+
+main()
